fcdba8d3168ef92506a2a2ede2296417296dd17a
Mi-kyuu <50164573+Mi-kyuu@users.noreply.github.com>
2020-09-25 18:14:35 +0900

    D136

--- ABC/D136.cpp
+++ ABC/D136.cpp
@@ -47,11 +47,7 @@ typedef vector<vector<ll> > vvll;
 #define debug_3(x1,x2,x3) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<endl
 #define debug_4(x1,x2,x3,x4) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<", "#x4<<": "<<x4<<endl
 #define debug_5(x1,x2,x3,x4,x5) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<", "#x4<<": "<<x4<<", "#x5<<": "<<x5<<endl
-#ifdef _DEBUG
 #define debug(...) CHOOSE((__VA_ARGS__,debug_5,debug_4,debug_3,debug_2,debug_1,~))(__VA_ARGS__)
-#else
-#define debug(...)
-#endif
 
 #define ynmes(a) (a) ? mes("Yes") : mes("No")
 #define YNmes(a) (a) ? mes("YES") : mes("NO")
