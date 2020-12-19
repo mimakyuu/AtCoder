520a6d8c7ab8aaadda142e5cf71cc0b3d5822fe1
Mi_kyuu <50164573+Mi-kyuu@users.noreply.github.com>
2020-12-19 12:31:44 +0900

    Template

---
+++ Kotlin/Template.kt
@@ -0,0 +1,16 @@
+import java.util.*
+fun readln() = readLine()!!
+fun readint() = readln().toInt()
+fun readlong() = readln().toLong()
+fun readdouble() = readln().toDouble()
+fun readstrings() = readln().split(" ")
+fun readints() = readstrings().map { it.toInt() }
+fun readlongs() = readstrings().map { it.toLong() }
+fun readdoubles() = readstrings().map { it.toDouble() }
+ 
+fun main() {
+    var tt = readint()
+    while (tt-- > 0) {
+        
+    }
+}
