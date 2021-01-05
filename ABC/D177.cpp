5f5931848cf8dcbc6de45082514da4766295a4d8
Mi_kyuu <50164573+Mi-kyuu@users.noreply.github.com>
2021-01-05 15:36:52 +0900

    Update UnionFind

--- ABC/D177.cpp
+++ ABC/D177.cpp
@@ -88,28 +88,30 @@ ll modncr(ll N, ll K) {
 
 
 struct UnionFind { //UnionFind
-    vector<int> par; // par[i]:iの親の番号 (例) par[3] = 2 : 3の親が2
+    vector<int> par;
+    
+    UnionFind(int n) : par(n, -1) { }
 
-    UnionFind(int N) : par(N) { //最初は全てが根であるとして初期化
-        for(int i = 0; i < N; i++) par[i] = i;
+    int root(int x) {
+        if (par[x] < 0) return x;
+        else return par[x] = root(par[x]);
     }
-
-    int root(int x) { // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
-        if (par[x] == x) return x;
-        return par[x] = root(par[x]);
+    
+    bool issame(int x, int y) {
+        return root(x) == root(y);
     }
-
-    void unite(int x, int y) { // xとyの木を併合
-        int rx = root(x); //xの根をrx
-        int ry = root(y); //yの根をry
-        if (rx == ry) return; //xとyの根が同じ(=同じ木にある)時はそのまま
-        par[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+    
+    bool merge(int x, int y) {
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (par[x] > par[y]) swap(x, y); // merge technique
+        par[x] += par[y];
+        par[y] = x;
+        return true;
     }
-
-    bool same(int x, int y) { // 2つのデータx, yが属する木が同じならtrueを返す
-        int rx = root(x);
-        int ry = root(y);
-        return rx == ry;
+    
+    int size(int x) {
+        return -par[root(x)];
     }
 };
 
@@ -123,7 +125,7 @@ signed main() {
     ll a, b;
     rep(i, m) {
         cin >> a >> b; a--; b--;
-        tree.unite(a, b);
+        tree.merge(a, b);
     }
     
     map <ll, ll> mp;
--- ARC/B106 - UnionFind.cpp
+++ ARC/B106 - UnionFind.cpp
@@ -88,29 +88,32 @@ ll modncr(ll N, ll K) {
 
 
 struct UnionFind { //UnionFind
-    vector<int> par; // par[i]:iの親の番号 (例) par[3] = 2 : 3の親が2
+    vector<int> par;
+    
+    UnionFind(int n) : par(n, -1) { }
 
-    UnionFind(int N) : par(N) { //最初は全てが根であるとして初期化
-        for(int i = 0; i < N; i++) par[i] = i;
+    int root(int x) {
+        if (par[x] < 0) return x;
+        else return par[x] = root(par[x]);
+    }
+    
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+    
+    bool merge(int x, int y) {
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (par[x] > par[y]) swap(x, y); // merge technique
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    
+    int size(int x) {
+        return -par[root(x)];
     }
 
-    int root(int x) { // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
-        if (par[x] == x) return x;
-        return par[x] = root(par[x]);
-    }
-
-    void unite(int x, int y) { // xとyの木を併合
-        int rx = root(x); //xの根をrx
-        int ry = root(y); //yの根をry
-        if (rx == ry) return; //xとyの根が同じ(=同じ木にある)時はそのまま
-        par[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
-    }
-
-    bool same(int x, int y) { // 2つのデータx, yが属する木が同じならtrueを返す
-        int rx = root(x);
-        int ry = root(y);
-        return rx == ry;
-    }
 };
 
 
@@ -132,7 +135,7 @@ signed main() {
     ll c, d;
     rep(i, m) {
         cin >> c >> d; c--; d--;
-        tree.unite(c, d);
+        tree.merge(c, d);
     }
     
     map <ll, ll> mp;
