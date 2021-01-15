1142b8a7f1131aacb36ba1d41beff4fc2fa57959
Mi_kyuu <50164573+Mi-kyuu@users.noreply.github.com>
2021-01-15 09:51:19 +0900

    E183

---
+++ ABC/E183 - QueenOnGrid - mint.cpp
@@ -0,0 +1,205 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+typedef long double ld;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pair<int, int> > vpii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<string> > vvs;
+typedef vector<vector<ll> > vvll;
+typedef vector<vector<bool> > vvb;
+
+#define rep(i, n) for (int i = 0; i < int(n); ++i)
+#define rrep(i, n) for (int i = 1; i <= int(n); ++i)
+#define irep(it, stl) for (auto it = stl.begin(); it != stl.end(); it++)
+#define drep(i, n) for (int i = int(n)-1; i >= 0; --i)
+
+#define MES(a) MES2 a
+#define MES2(a0,a1,a2,a3,a4,x,...) x
+#define mes_1(x1) cout<<x1<<endl
+#define mes_2(x1,x2) cout<<x1<<" "<<x2<<endl
+#define mes_3(x1,x2,x3) cout<<x1<<" "<<x2<<" "<<x3<<endl
+#define mes_4(x1,x2,x3,x4) cout<<x1<<" "<<x2<<" "<<x3<<" "<<x4<<endl
+#define mes_5(x1,x2,x3,x4,x5) cout<<x1<<" "<<x2<<" "<<x3<<" "<<x4<<" "<<x5<<endl
+#define mes(...) CHOOSE((__VA_ARGS__,mes_5,mes_4,mes_3,mes_2,mes_1,~))(__VA_ARGS__)
+
+#define CHOOSE(a) CHOOSE2 a
+#define CHOOSE2(a0,a1,a2,a3,a4,x,...) x
+#define debug_1(x1) cout<<#x1<<": "<<x1<<endl
+#define debug_2(x1,x2) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<endl
+#define debug_3(x1,x2,x3) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<endl
+#define debug_4(x1,x2,x3,x4) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<", "#x4<<": "<<x4<<endl
+#define debug_5(x1,x2,x3,x4,x5) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<", "#x4<<": "<<x4<<", "#x5<<": "<<x5<<endl
+#define debug(...) CHOOSE((__VA_ARGS__,debug_5,debug_4,debug_3,debug_2,debug_1,~))(__VA_ARGS__)
+
+#define ynmes(a) (a) ? mes("Yes") : mes("No")
+#define YNmes(a) (a) ? mes("YES") : mes("NO")
+#define re0 return 0
+#define mp(p, q) make_pair(p, q)
+#define pb(n) push_back(n)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define Sort(a) sort(a.begin(), a.end())
+#define rSort(a) sort(a.rbegin(), a.rend())
+#define Rev(a) reverse(a.begin(), a.end())
+#define MATHPI acos(-1)
+#define itn int;
+int dx[8] = { 1, 0, -1, 0, 1, -1, -1, 1 };
+int dy[8] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+template <class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+struct io { io() { ios::sync_with_stdio(false); cin.tie(0); } };
+const int INF = INT_MAX;
+const ll LLINF = 1LL << 60;
+const ll MOD = 1000000007;
+const double EPS = 1e-9;
+
+ll modpow(ll x, ll y) {
+    if(y == 1) {
+        return x;
+    }
+    ll ans;
+    if(y % 2 == 1) {
+        ll r = modpow(x,(y-1)/2);
+        ans = r * r % MOD;
+        ans = ans * x % MOD;
+    }
+    else {
+        ll r = modpow(x,y/2);
+        ans = r * r % MOD;
+    }
+    return ans;
+}
+
+ll modncr(ll N, ll K) {
+    ll res = 1;
+    ll p=1;
+    for (ll n = 0; n < K; ++n) {
+      res = (res*(N - n))%MOD;
+      p = (p*(n + 1))%MOD;
+    }
+    return (res*modpow(p,MOD-2))%MOD;
+}
+
+
+//mint
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
+    constexpr int getmod() const { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b, swap(a, b);
+            u -= t * v, swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr istream& operator >> (istream& is, Fp<MOD>& x) noexcept {
+        is >> x.val;
+        x.val %= MOD;
+        if (x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream& os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD>& r, long long n) noexcept {
+        if (n == 0) return 1;
+        if (n < 0) return modpow(modinv(r), -n);
+        auto t = modpow(r, n / 2);
+        t = t * t;
+        if (n & 1) t = t * r;
+        return t;
+    }
+    friend constexpr Fp<MOD> modinv(const Fp<MOD>& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b, swap(a, b);
+            u -= t * v, swap(u, v);
+        }
+        return Fp<MOD>(u);
+    }
+};
+
+using mint = Fp<MOD>; //MOD = 1e9+7
+
+
+signed main() {
+	ll h, w;
+    cin >> h >> w;
+    vs field(h);
+    rep(i, h) {
+		cin >> field[i];
+	}
+
+    vector<vector<mint>> dp(h+1, vector<mint>(w+1, 0)), x = dp, y = dp, z = dp;
+    dp[1][1] = 1;
+	
+    rrep(i, h) {
+        rrep(j, w) {
+            // 壁だったらこれまでの累積和をリセット
+            if (field[i-1][j-1] == '#') {
+                x[i][j] = y[i][j] = z[i][j] = 0;
+                continue;
+            }
+            // 三方向からの累積和を合算
+            dp[i][j] += x[i][j-1] + y[i-1][j] + z[i-1][j-1];
+            
+            // 累積和を更新
+            x[i][j] = x[i][j-1] + dp[i][j];
+            y[i][j] = y[i-1][j] + dp[i][j];
+            z[i][j] = z[i-1][j-1] + dp[i][j];
+        }
+    }
+    mes(dp[h][w]);
+	
+	
+	
+	
+	
+	
+	
+}
