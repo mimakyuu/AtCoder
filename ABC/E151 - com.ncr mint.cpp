b35e982245581b4ca9ee4ac2c6b6c3f262fd44d2
Mi_kyuu <50164573+mimakyuu@users.noreply.github.com>
2021-09-23 20:50:28 +0900

    E151

---
+++ ABC/E151 - com.ncr mint.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+using namespace std;
+
+//#define ACL
+#ifdef ACL
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+#if __has_include("acl-all.h")
+#include "acl-all.h" //on Wandbox
+using namespace atcoder;
+#endif
+#endif //#ifdef ACL
+
+typedef long double ld;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pair<ll, ll> > vpll;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<string> > vvs;
+typedef vector<vector<ll> > vvll;
+typedef vector<vector<bool> > vvb;
+typedef pair<ll, ll> pll;
+
+#define rep(i, n) for (int i = 0; i < int(n); ++i)
+#define rrep(i, n) for (int i = 1; i <= int(n); ++i)
+#define irep(it, stl) for (auto it = stl.begin(); it != stl.end(); it++)
+#define drep(i, n) for (int i = int(n)-1; i >= 0; --i)
+
+#define MES(a) MES2 a
+#define MES2(a0,a1,a2,a3,a4,x,...) x
+#define mes_1(x1) cout<<x1<<endl
+#define mes_2(x1,x2) cout<<x1<<" "<<x2<<endl
+#define mes_3(x1,x2,x3) cout<<x1<<" "<<x2<<" "<<x3<<endl
+#define mes_4(x1,x2,x3,x4) cout<<x1<<" "<<x2<<" "<<x3<<" "<<x4<<endl
+#define mes_5(x1,x2,x3,x4,x5) cout<<x1<<" "<<x2<<" "<<x3<<" "<<x4<<" "<<x5<<endl
+#define mes(...) CHOOSE((__VA_ARGS__,mes_5,mes_4,mes_3,mes_2,mes_1,~))(__VA_ARGS__)
+
+#define CHOOSE(a) CHOOSE2 a
+#define CHOOSE2(a0,a1,a2,a3,a4,x,...) x
+#define debug_1(x1) cout<<#x1<<": "<<x1<<endl
+#define debug_2(x1,x2) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<endl
+#define debug_3(x1,x2,x3) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<endl
+#define debug_4(x1,x2,x3,x4) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<", "#x4<<": "<<x4<<endl
+#define debug_5(x1,x2,x3,x4,x5) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<", "#x4<<": "<<x4<<", "#x5<<": "<<x5<<endl
+#define debug(...) CHOOSE((__VA_ARGS__,debug_5,debug_4,debug_3,debug_2,debug_1,~))(__VA_ARGS__)
+
+#define Ynmes(a) (a) ? mes("Yes") : mes("No")
+#define YNmes(a) (a) ? mes("YES") : mes("NO")
+#define re0 return 0
+#define mp(p, q) make_pair(p, q)
+#define pb(n) push_back(n)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define Sort(a) sort(a.begin(), a.end())
+#define rSort(a) sort(a.rbegin(), a.rend())
+#define Rev(a) reverse(a.begin(), a.end())
+
+int dx[8] = { 1, 0, -1, 0, 1, -1, -1, 1 };
+int dy[8] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+template <class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+struct io { io() { ios::sync_with_stdio(false); cin.tie(0); } };
+const int INF = INT_MAX;
+const ll LLINF = 1LL << 60;
+const ll MOD = 1000000007;
+const double EPS = 1e-9;
+
+//ModInt
+template<int MOD> struct ModInt {
+    static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+    ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    int get() const { return (int)x; }
+    ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+    ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+    ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+    ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+    ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+    ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+    ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+        while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+        return ModInt(u); }
+    bool operator==(ModInt that) const { return x == that.x; }
+    bool operator!=(ModInt that) const { return x != that.x; }
+    ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+    ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+template<typename T, int FAC_MAX> struct Comb { vector<T> fac, ifac;
+    Comb(){fac.resize(FAC_MAX,1);ifac.resize(FAC_MAX,1);rrep(i,FAC_MAX)fac[i]=fac[i-1]*i;
+        ifac[FAC_MAX-1]=T(1)/fac[FAC_MAX-1];for(int i = FAC_MAX-2; i >= 1; i--)ifac[i]=ifac[i+1]*T(i+1);}
+    T npr(int a, int b) { if (b < 0 || a < b) return T(0); return fac[a] * ifac[a - b]; }
+    T ncr(int a, int b) { if (b < 0 || a < b) return T(0); return fac[a] * ifac[a - b] * ifac[b]; }
+    T nhk(int n, int k) { if (n == 0 && k == 0) return T(1); if (n <= 0 || k < 0) return 0;
+        return ncr(n + k - 1, k); } // nHk = (n+k-1)Ck : n is separator
+    T pairCombination(int n) {if(n%2==1)return T(0);return fac[n]*ifac[n/2]/(T(2)^(n/2));}
+    // combination of paris for n
+}; 
+typedef ModInt<1000000007> mint;
+//ModInt End
+
+signed main() {
+	ll n, k;
+	cin >> n >> k;
+	vll a(n);
+	rep(i, n) {
+		cin >> a[i];
+	}
+	
+	Comb<mint, 200005> com;
+	
+	mint ma = 0, mi = 0;
+	Sort(a);
+	for (int i = k-1; i < n; i++) {
+		ma += mint(a[i]) * com.ncr(i, k-1);
+	}
+	rSort(a);
+	for (int i = k-1; i < n; i++) {
+		mi += mint(a[i]) * com.ncr(i, k-1);
+	}
+	
+	mes(ma-mi);
+	
+	
+	
+}
